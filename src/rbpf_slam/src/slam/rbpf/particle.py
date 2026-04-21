4b47730a1dbbaba20e555154373c872700c5613b
es <smid.eiko@gmail.com>
2026-04-21 20:25:26 +0200

    Implemented class particle filter to rbpf.

---
+++ src/rbpf_slam/src/slam/rbpf/particle.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+from typing import Tuple
+import numpy as np
+
+
+from slam.infrascruture.defs import Pose2D
+from slam.scan_matcher.scan_matcher import ScanMatcher
+
+
+class Particle:
+    '''
+    Class representing a single particle.  
+    '''
+    def __init__(self, pose: Pose2D, weight: float, scan_matcher: ScanMatcher):
+        self.pose = pose
+        self.weight = weight
+        self.scan_matcher = scan_matcher
+
+
+    def copy(self):
+        '''
+        Creates a copy of the particle. The scan matcher is also copied to ensure that the particles are
+        independent of each other.
+        '''
+        return Particle(
+            pose=self.pose,
+            weight=self.weight,
+            scan_matcher=self.scan_matcher.copy()
+        )
